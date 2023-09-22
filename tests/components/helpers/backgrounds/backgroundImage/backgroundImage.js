d6ab54b4f0db9ecfc3bd244bfe44c1927c402f27
Christopher Salvo <chphsalvo@gmail.com>
2023-09-22 16:10:14 +0200

    wip

--- tests/components/helpers/backgrounds/backgroundImage/backgroundImage.js
+++ tests/components/helpers/backgrounds/backgroundImage/backgroundImage.js
@@ -2,16 +2,13 @@ import styles from './backgroundImage.module.scss'
 
 export default function BackgroundImage() {
   return (
-      <div className="flex middle">
-          <div className={styles['bg-img'] + ' col col-x2 bg-img'}
-              style={{
-                  backgroundImage: "url('https://picsum.photos/id/257/1920/1080')",
-                }}></div>
-
-          <div className={styles['bg-img'] + ' col col-x2 bg-img'}
-          style={{
-                  backgroundImage: "url('https://picsum.photos/id/257/1920/1080')",
-                }}></div>
+    <div className="flex middle">
+      <div className="col col-x2">
+        <div className="bg-img aspect-square" style={{ backgroundImage: "url('https://picsum.photos/id/257/1920/1080')" }}></div>
       </div>
+      <div className="col col-x2">
+        <div className="bg-img aspect-video" style={{ backgroundImage: "url('https://picsum.photos/id/257/1920/1080')" }}></div>
+      </div>
+    </div>
   )
 }
